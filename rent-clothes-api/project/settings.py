3ebd915491ccbcfee2607656e66bd0553edffc26
Anna Monkman <annamonkman@me.com>
2021-04-15 15:57:04 +0100

    adding authentication

--- rent-clothes-api/project/settings.py
+++ rent-clothes-api/project/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'items'
+    'items',
+    'jwt_auth'
 ]
 
 MIDDLEWARE = [
@@ -127,3 +128,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'jwt_auth.User'
