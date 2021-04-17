546337ce54111ba356703ca6e6763f8d886eca27
Anna Monkman <annamonkman@me.com>
2021-04-17 16:06:04 +0100

    adding functionality to register and login forms

--- rent-clothes-api/client/src/components/Login.js
+++ rent-clothes-api/client/src/components/Login.js
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  })
+  console.log(formData, setFormData)
+  
   return (
     <div className="login-form-container">
       <form>
@@ -11,6 +17,9 @@ const Login = () => {
         <div className="form-field">
           <label htmlFor="password">Password</label>
           <input name="password" type="password" id="password"/>
+        </div>
+        <div className="form-field">
+          <input className="submit-button" type="submit" value="Login"/>
         </div>
       </form>
     </div>
--- rent-clothes-api/client/src/components/Register.js
+++ rent-clothes-api/client/src/components/Register.js
@@ -10,35 +10,74 @@ const Register = () => {
     password_confirmation: '',
   })
   console.log(formData, setFormData)
+
+  const handleChange = (event) => {
+    // set state when user types
+
+  }
   
   return (
     <div className="register-form-container">
       <form>
         <div className="form-field">
           <label htmlFor="username">Username</label>
-          <input name="username" type="text" id="username"/>
+          <input 
+            name="username" 
+            type="text" id="username" 
+            value={formData.username} 
+            onChange={handleChange}/>
         </div>
         <div className="form-field">
           <label htmlFor="firstname">First Name</label>
-          <input name="first_name" type="text" id="firstname" />
+          <input 
+            name="first_name" 
+            type="text" 
+            id="firstname" 
+            value={formData.first_name} 
+            onChange={handleChange}/>
         </div>
         <div className="form-field">
           <label htmlFor="lastname">Last Name</label>
-          <input name="last_name" type="text" id="lastname"/>
+          <input 
+            name="last_name" 
+            type="text" 
+            id="lastname" 
+            value={formData.last_name} 
+            onChange={handleChange}/>
         </div>
         <div className="form-field">
           <label htmlFor="email">Email Address</label>
-          <input name="email" type="email" id="email"/>
+          <input 
+            name="email" 
+            type="email" 
+            id="email" 
+            value={formData.email} 
+            onChange={handleChange}/>
         </div>
         <div className="form-field">
           <label htmlFor="password">Password</label>
-          <input name="password" type="password" id="password"/>
+          <input 
+            name="password" 
+            type="password" 
+            id="password" 
+            value={formData.password} 
+            onChange={handleChange}/>
         </div>
         <div className="form-field">
           <label htmlFor="passwordconf">Confirm Your Password</label>
-          <input name="password_confirmation" type="password" id="passwordconf"/>
+          <input 
+            name="password_confirmation" 
+            type="password" 
+            id="passwordconf" 
+            value={formData.password_confirmation} 
+            onChange={handleChange}/>
         </div>
-        
+        <div className="form-field">
+          <input 
+            className="submit-button" 
+            type="submit" 
+            value="Register"/>
+        </div>
       </form>
     </div>
   )
