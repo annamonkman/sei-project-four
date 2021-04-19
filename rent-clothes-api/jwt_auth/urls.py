0b714922e4b4418c3488c42cf21995abbcbe2f40
Anna Monkman <annamonkman@me.com>
2021-04-19 14:03:44 +0100

    starting wishlist

--- rent-clothes-api/jwt_auth/urls.py
+++ rent-clothes-api/jwt_auth/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path 
-from .views import RegisterView, LoginView, UserDetailView, UserListView
+from .views import RegisterView, LoginView, UserDetailView, UserListView, WishlistView
 
 urlpatterns = [
     path('register/', RegisterView.as_view()),
     path('login/', LoginView.as_view()),
     path('', UserListView.as_view()),
-    path('<int:pk>/', UserDetailView.as_view())
+    path('<int:pk>/', UserDetailView.as_view()),
+    path('<int:pk>/wishlist/', WishlistView.as_view())
 ]
--- rent-clothes-api/jwt_auth/views.py
+++ rent-clothes-api/jwt_auth/views.py
@@ -10,6 +10,9 @@ from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from .serializers.common import UserSerializer
 
+# import logging
+# log = logging.getLogger(__name__)
+
 User = get_user_model()
 
 class RegisterView(APIView):
@@ -55,6 +58,7 @@ class UserListView(APIView):
     
     def get(self, _request):
         users = User.objects.all()
+        print(users)
         serialized_users = UserSerializer(users, many=True)
         return Response(serialized_users.data, status=status.HTTP_200_OK)
 
@@ -68,12 +72,32 @@ class UserDetailView(APIView):
 
     def get(self, _request, pk):
         user = self.get_user(pk=pk)
+        print(user)
         serialized_user = UserSerializer(user)
         return Response(serialized_user.data, status=status.HTTP_200_OK)
 
 
 
-# class WishlistView(APIView):
+class WishlistView(APIView):
+    # def post_product(self, _request, pk):
+    #     get_user_id = User.objects.get(pk=pk)
+
+    #     user_id = request.data.get('id')
+
+    #     print('get user id>>>>>', get_user_id)
+    #     print('user id >>>>>', user_id)
+    #     print('HELLO')
+
+    def put(self, _request, pk):
+        user = User.objects.get(pk=pk)
+        product = request.data.get('id')
+
+        print('get user id>>>>>', user)
+        print('HELLO')
+
+        
+
+        # add product to wishlist that is on user and then save it
 
 # def addproduct(request, id):
 #     item=get_object_or_404(Product, id=id)
