c95eb36fa9eecc70b9a9412138198f5941a95db9
Anna Monkman <annamonkman@me.com>
2021-08-25 16:35:40 +0100

    wokring app but need to fix bugs

--- rent-clothes-api/jwt_auth/views.py
+++ rent-clothes-api/jwt_auth/views.py
@@ -2,11 +2,13 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.exceptions import PermissionDenied
+from rest_framework.exceptions import NotFound
 from django.contrib.auth import get_user_model
 from datetime import datetime, timedelta
 from django.conf import settings
 import jwt
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+# from keystoneclient.exceptions import NotFound
 
 from .serializers.common import UserSerializer
 
